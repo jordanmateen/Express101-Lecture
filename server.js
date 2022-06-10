33e52fc15f535298a158852c770d7ba70a2ffad1
Jordan Mateen <jordan.mateen@coxautoinc.com>
2022-06-10 17:21:02 -0400

    adding es6 templating

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const es6Renderer = require('express-es6-template-engine');
 const app = express();
 const PORT = 3000;
 
@@ -10,9 +11,24 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// get route. Home route
+// express-es6-template-engine set up
+app.engine('html', es6Renderer); // Registers HTML as the engine (this is what type of view will be rendering. )
+app.set('views', 'templates'); // The templates folder is where our "views" will be located. 
+app.set('view engine', 'html'); // the engine we set up on line 15 will be used here. 
+
+// get route, home route (this page will be served using line 8)
 app.get('/', (req, res)=>{
-    res.send('hello world, How are you today?');
+    res.send('Hello World, How are you today?');
+})
+
+// get route, home route using es6 template  (this page will be served using the es 6 temolate setup line 15-17)
+app.get('/home', (req, res)=>{
+    res.render('home', {
+        locals: {
+            title: "Home Page",
+            description: "A blank canvas depicts a white rabbit in a snowstorm. Now that's art!!!"
+        }
+    });
 })
 
 //get route
